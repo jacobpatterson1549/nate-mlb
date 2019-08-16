8e50c79d56421273b74ae6b192a877fac4f5fa10
Jacob Patterson <jacob.patterson@gmail.com>
2019-08-16 09:36:44 -0700

    better error message

--- internal/server/export.go
+++ internal/server/export.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"encoding/csv"
+	"fmt"
 	"io"
 	"nate-mlb/internal/request"
 )
@@ -11,5 +12,9 @@ func exportToCsv(es request.EtlStats, w io.Writer) error {
 	records[0] = []string{"type", "friend", "value", "player", "score"}
 
 	csvWriter := csv.NewWriter(w)
-	return csvWriter.WriteAll(records)
+	err := csvWriter.WriteAll(records)
+	if err != nil {
+		return fmt.Errorf("problem writing to csv: %v", err)
+	}
+	return nil
 }
