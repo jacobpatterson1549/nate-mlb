6e7e384fafc6e4d0a726e9c25d9c3c6eec39b171
Jacob Patterson <jacob.patterson@gmail.com>
2019-08-16 09:46:26 -0700

    adding stats to csv export

--- internal/server/export.go
+++ internal/server/export.go
@@ -4,12 +4,32 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"nate-mlb/internal/db"
 	"nate-mlb/internal/request"
+	"strconv"
 )
 
 func exportToCsv(es request.EtlStats, w io.Writer) error {
-	records := make([][]string, 1)
-	records[0] = []string{"type", "friend", "value", "player", "score"}
+	records := make([][]string, 2)
+	records[0] = []string{"nate-mlb", "2019"}
+	records[1] = []string{"type", "friend", "value", "player", "score"}
+	for i, sc := range es.ScoreCategories {
+		for _, fs := range sc.FriendScores {
+			for k, ps := range fs.PlayerScores {
+				record := make([]string, 5)
+				if i == 0 {
+					record[0] = db.PlayerType(sc.PlayerTypeID).Name()
+				}
+				if k == 0 {
+					record[1] = fs.FriendName
+					record[2] = strconv.Itoa(fs.Score)
+				}
+				record[3] = ps.PlayerName
+				record[4] = strconv.Itoa(ps.Score)
+				records = append(records, record)
+			}
+		}
+	}
 
 	csvWriter := csv.NewWriter(w)
 	err := csvWriter.WriteAll(records)
