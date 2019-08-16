00aa69c73dcddb372f990a86812a8eaf26f04ee9
Jacob Patterson <jacob.patterson@gmail.com>
2019-08-16 10:56:11 -0700

    adding helper funcions to simplify record/records creation

--- internal/server/export.go
+++ internal/server/export.go
@@ -15,6 +15,16 @@ func exportToCsv(es request.EtlStats, w io.Writer) error {
 		return err
 	}
 
+	csvWriter := csv.NewWriter(w)
+	records := createRecords(es)
+	err = csvWriter.WriteAll(records)
+	if err != nil {
+		return fmt.Errorf("problem writing to csv: %v", err)
+	}
+	return nil
+}
+
+func createRecords(es request.EtlStats) [][]string {
 	records := make([][]string, 3)
 	records[0] = []string{"nate-mlb", "2019"}
 	records[2] = []string{"type", "friend", "value", "player", "score"}
@@ -25,25 +35,24 @@ func exportToCsv(es request.EtlStats, w io.Writer) error {
 		for j, fs := range sc.FriendScores {
 			records = append(records, nil)
 			for k, ps := range fs.PlayerScores {
-				record := make([]string, 5)
-				if j == 0 && k == 0 {
-					record[0] = sc.PlayerTypeID.Name()
-				}
-				if k == 0 {
-					record[1] = fs.FriendName
-					record[2] = strconv.Itoa(fs.Score)
-				}
-				record[3] = ps.PlayerName
-				record[4] = strconv.Itoa(ps.Score)
+				record := createRecord(sc, fs, ps, j, k)
 				records = append(records, record)
 			}
 		}
 	}
+	return records
+}
 
-	csvWriter := csv.NewWriter(w)
-	err = csvWriter.WriteAll(records)
-	if err != nil {
-		return fmt.Errorf("problem writing to csv: %v", err)
+func createRecord(sc request.ScoreCategory, fs request.FriendScore, ps request.PlayerScore, fsIndex, psIndex int) []string {
+	record := make([]string, 5)
+	if psIndex == 0 {
+		if fsIndex == 0 {
+			record[0] = sc.PlayerTypeID.Name()
+		}
+		record[1] = fs.FriendName
+		record[2] = strconv.Itoa(fs.Score)
 	}
-	return nil
+	record[3] = ps.PlayerName
+	record[4] = strconv.Itoa(ps.Score)
+	return record
 }
