b502e78caa249ffde345ba49a68efcb22049f608
Jacob Patterson <jacob.patterson@gmail.com>
2019-08-17 18:52:51 -0700

    moving ScoreCategorizer to stats.go

--- internal/request/stats.go
+++ internal/request/stats.go
@@ -7,6 +7,19 @@ import (
 	"strings"
 )
 
+// ScoreCategorizers maps PlayerTypes to ScoreCategorizers for them
+var ScoreCategorizers = map[db.PlayerType]ScoreCategorizer{
+	db.PlayerTypeTeam:    mlbTeamRequestor{},
+	db.PlayerTypeHitter:  mlbPlayerRequestor{playerType: db.PlayerTypeHitter},
+	db.PlayerTypePitcher: mlbPlayerRequestor{playerType: db.PlayerTypePitcher},
+}
+
+// ScoreCategorizer requests data for and creates a ScoreCategory for the FriendPlayerInfo
+// TODO: move to stats.go
+type ScoreCategorizer interface {
+	RequestScoreCategory(fpi FriendPlayerInfo, pt db.PlayerType) (ScoreCategory, error)
+}
+
 // ScoreCategory contain the FriendScores for each PlayerType
 type ScoreCategory struct {
 	Name         string
