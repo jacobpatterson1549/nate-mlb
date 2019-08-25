b155ba8c6f25ef44e544fedfaf28fff8dd7abf3b
Jacob Patterson <jacob.patterson@gmail.com>
2019-08-25 11:16:54 -0700

    tweaking how main init works

--- main.go
+++ main.go
@@ -10,33 +10,44 @@ import (
 	_ "github.com/lib/pq"
 )
 
+// names of variables which should be specied in the runtime environment or be in the .env file
+const (
+	EnvironmentVariableDatabaseURL = "DATABASE_URL"
+	EnvironmentVariablePort        = "PORT"
+)
+
 var (
 	driverName     string
 	dataSourceName string
-	port           string
+	portNumber     int
 )
 
 func init() {
-	driverName = "postgres"
 	var ok bool
+
+	driverName = "postgres"
+
 	dataSourceName, ok = os.LookupEnv("DATABASE_URL")
 	if !ok {
-		log.Fatal("DATABASE_URL environment variable not set")
+		log.Fatal(EnvironmentVariableDatabaseURL, "environment variable not set")
 	}
-	port, ok = os.LookupEnv("PORT")
+
+	var port string
+	port, ok = os.LookupEnv(EnvironmentVariablePort)
 	if !ok {
-		log.Fatal("PORT environment variable not set")
+		log.Fatal(EnvironmentVariablePort, "environment variable not set")
+	}
+	var err error
+	portNumber, err = strconv.Atoi(port)
+	if err != nil {
+		log.Fatal(EnvironmentVariablePort, "invalid number:", port)
 	}
 }
 
 func main() {
 	err := db.Init(driverName, dataSourceName)
 	if err != nil {
-		log.Fatal("Could not set database ", err)
-	}
-	portNumber, err := strconv.Atoi(port)
-	if err != nil {
-		log.Fatalf("PORT (%v) invalid as number: %v", port, err)
+		log.Fatal(err)
 	}
 
 	err = server.Run(portNumber)
