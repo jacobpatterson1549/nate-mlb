0b90206e462f975e5b9cbc75e35ea476a6d29973
Jacob Patterson <jacob.patterson@gmail.com>
2019-09-04 14:33:34 -0700

    Removing TODO: views are not used; SETOF RECORD with named OUT params are used instead

--- sql/functions/clr/clr_year_active.sql
+++ sql/functions/clr/clr_year_active.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE FUNCTION clr_year_active(sport_type_id INT) RETURNS BOOLEAN
 AS $$ BEGIN
-UPDATE stats AS s -- TODO: update years view instead, rename to years_view
+UPDATE stats AS s
 SET active = NULL
 WHERE s.active
 AND s.sport_type_id = clr_year_active.sport_type_id;
