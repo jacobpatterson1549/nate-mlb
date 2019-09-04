89cc4161b75f7c0fac82d86f47fec45be905e0b2
Jacob Patterson <jacob.patterson@gmail.com>
2019-09-04 10:46:40 -0700

    fixing parameter

--- sql/functions/get_players.sql
+++ sql/functions/get_players.sql
@@ -6,7 +6,7 @@ FROM stats AS s
 JOIN friends AS f ON s.year = f.year AND s.sport_type_id = f.sport_type_id
 JOIN players AS p ON f.id = p.friend_id
 WHERE s.active
-AND s.sport_type_id = $1
+AND s.sport_type_id = get_players.sport_type_id
 ORDER BY p.player_type_id ASC, p.friend_id ASC, p.display_order ASC;
 END $$
 LANGUAGE plpgsql;
