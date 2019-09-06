6f3d7791bfc76d498f5fbe7a984467a7e76a12d8
Jacob Patterson <jacob.patterson@gmail.com>
2019-09-06 16:28:22 -0700

    moving years form  javascript to js file

---
+++ js/admin-form-inputs/years.js
@@ -0,0 +1,60 @@
+function removeYear() {
+    var year = event.target.parentNode;
+    var removedYearChecked = year.querySelector('.year-radio').checked;
+    year.remove();
+    if (removedYearChecked) {
+        var yearsParent = document.getElementById('years');
+        var yearRadios = yearsParent.querySelectorAll('.year-radio');
+        if (yearRadios.length > 0) {
+            yearRadios[yearRadios.length - 1].checked = true;
+        }
+    }
+}
+
+function addYear() {
+    var yearsParent = document.getElementById('years');
+    var year = document.getElementById('add-year-input').value;
+    var yearInputs = yearsParent.querySelectorAll('.year-input');
+    for (var i = 0; i < yearInputs.length; i++) {
+        var yearI = yearInputs[i].value;
+        if (yearI === year) {
+            return;
+        }
+    }
+    var newYear = createYear(year, "true");
+    newYear.querySelector('.year-radio').focus();
+}
+
+function createYear(yearNum, active) {
+    var template = document.getElementById("year-template");
+    var clone = document.importNode(template.content, true);
+    var newYear = clone.querySelector('.form-check');
+    newYear.id = 'year-' + yearNum;
+    newYear.querySelector('.year-radio').id = 'year-' + yearNum + '-active';
+    newYear.querySelector('.year-radio').value = yearNum;
+    if (active == "true") {
+        newYear.querySelector('.year-radio').checked = true;
+    }
+    newYear.querySelector('.year-label').htmlFor = 'year-' + yearNum + '-active';
+    newYear.querySelector('.year-label').innerText = yearNum;
+    newYear.querySelector('.year-input').value = yearNum;
+    var years = document.getElementById("years");
+    years.appendChild(clone);
+    return newYear;
+}
+
+function initYears() {
+    var years = document.getElementById("years").children;
+    for (var i = 0; i < years.length; i++) {
+        var value = years[i].querySelector(".value").innerText;
+        var active = years[i].querySelector(".active").innerText;
+        var newYear = createYear(value, active);
+        years[i].replaceWith(newYear);
+    }
+}
+function initAddYearInput() {
+    document.getElementById('add-year-input').value = new Date().getYear() + 1900;
+}
+
+initYears();
+initAddYearInput();
