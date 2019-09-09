0428266e3901fd5caacdbf3f72c1ac26c07b8f85
Jacob Patterson <jacob.patterson@gmail.com>
2019-09-09 13:06:37 -0700

    storing relative path in attribute which does not get mutated, so script can be run multiple times

--- js/statsTab.js
+++ js/statsTab.js
@@ -2,7 +2,7 @@ var statsTab = {
     init: function () {
         var statsAdminLinks = document.querySelectorAll(".stats-admin-link");
         for (var i = 0; i < statsAdminLinks.length; i++) {
-            statsAdminLinks[i].href = location.pathname + statsAdminLinks[i].pathname;
+            statsAdminLinks[i].href = location.pathname + statsAdminLinks[i].getAttribute('data-relative-path');
         }
     },
 };
