4db0142124f81931cb3920711334324435718224
Jacob Patterson <jacob.patterson@gmail.com>
2019-09-14 11:20:49 -0700

    adding CommandLine flag parsing

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"log"
 	"os"
 	"strconv"
 
+	"github.com/jacobpatterson1549/nate-mlb/go/db"
 	"github.com/jacobpatterson1549/nate-mlb/go/server"
 	_ "github.com/lib/pq"
 )
@@ -18,32 +21,45 @@ var (
 	databaseDriverName string
 	dataSourceName     string
 	portNumber         int
+	adminPassword      string
 )
 
 func init() {
-	var ok bool
-
 	databaseDriverName = "postgres"
+	flag.StringVar(&dataSourceName, "ds", "", "The data source to the PostgreSQL database (connection URI).  Defaults to "+environmentVariableDatabaseURL)
+	flag.IntVar(&portNumber, "p", 0, "The port number to run the server on.  Defaults to "+environmentVariablePort)
+	flag.StringVar(&adminPassword, "ap", "", "The admin user password.  Requires the -ds option.")
 
-	dataSourceName, ok = os.LookupEnv(environmentVariableDatabaseURL)
-	if !ok {
-		log.Fatal(environmentVariableDatabaseURL, " environment variable not set")
+	fmt.Println(databaseDriverName, dataSourceName, portNumber, adminPassword)
+	var ok bool
+	if len(dataSourceName) == 0 {
+		dataSourceName, ok = os.LookupEnv(environmentVariableDatabaseURL)
+		if !ok {
+			log.Fatal(environmentVariableDatabaseURL, " environment variable not set")
+		}
 	}
-
-	var port string
-	port, ok = os.LookupEnv(environmentVariablePort)
-	if !ok {
-		log.Fatal(environmentVariablePort, " environment variable not set")
-	}
-	var err error
-	portNumber, err = strconv.Atoi(port)
-	if err != nil {
-		log.Fatal(environmentVariablePort, " environment variable is invalid: ", port)
+	if len(adminPassword) == 0 && portNumber == 0 {
+		var port string
+		port, ok = os.LookupEnv(environmentVariablePort)
+		if !ok {
+			log.Fatal(environmentVariablePort, " environment variable not set")
+		}
+		var err error
+		portNumber, err = strconv.Atoi(port)
+		if err != nil {
+			log.Fatal(environmentVariablePort, " environment variable is invalid: ", port)
+		}
 	}
 }
 
 func main() {
-	err := server.Run(portNumber, databaseDriverName, dataSourceName)
+	var err error
+	switch {
+	case len(adminPassword) != 0:
+		err = db.SetAdminPassword(adminPassword)
+	default:
+		err = server.Run(portNumber, databaseDriverName, dataSourceName)
+	}
 	if err != nil {
 		log.Fatal(err)
 	}
