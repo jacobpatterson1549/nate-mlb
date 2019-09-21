7195b1b462228e439f5fdd1f52cc8cb7fdea765e
Jacob Patterson <jacob.patterson@gmail.com>
2019-09-21 09:28:07 -0700

    adding TestNflTeamRequestScoreCategory

--- go/request/nfl_team_test.go
+++ go/request/nfl_team_test.go
@@ -1,6 +1,73 @@
 package request
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+
+	"github.com/jacobpatterson1549/nate-mlb/go/db"
+)
+
+func TestNflTeamRequestScoreCategory(t *testing.T) {
+	requestScoreCategoryTests := []struct {
+		friends   []db.Friend
+		players   []db.Player
+		teamsJSON string
+		wantErr   bool
+		want      ScoreCategory
+	}{
+		{
+			wantErr: true, // no teamsJSON
+		},
+		{
+			teamsJSON: `{"nflTeams":{"30":{"nflTeamId":"30","fullName":"Seattle Seahawks","record":"all-0-0"}}}`,
+			wantErr:   true, // bad wins
+		},
+		{
+			friends: []db.Friend{{ID: 7, DisplayOrder: 1, Name: "Anthony"}},
+			players: []db.Player{
+				{ID: 4, SourceID: 30, FriendID: 7, DisplayOrder: 1}, // Seattle Seahawks 10
+				{ID: 1, SourceID: 20, FriendID: 7, DisplayOrder: 3}, // Minnesota Vikings 8
+				{ID: 3, SourceID: 29, FriendID: 7, DisplayOrder: 2}, // San Francisco 49er 4
+			},
+			// TODO: don't read "nflTeamId" field on map
+			teamsJSON: `{"nflTeams":{
+				"20":{"nflTeamId":"20","fullName":"Minnesota Vikings","record":"8-7-1"},
+				"29":{"nflTeamId":"29","fullName":"San Francisco 49ers","record":"4-12-0"},
+				"30":{"nflTeamId":"30","fullName":"Seattle Seahawks","record":"10-6-0"}}}`,
+			want: ScoreCategory{
+				PlayerType: db.PlayerTypeNflTeam,
+				FriendScores: []FriendScore{
+					{
+						DisplayOrder: 1, ID: 7, Name: "Anthony", Score: 22,
+						PlayerScores: []PlayerScore{
+							{ID: 4, Name: "Seattle Seahawks", Score: 10, DisplayOrder: 1, SourceID: 30},
+							{ID: 3, Name: "San Francisco 49ers", Score: 4, DisplayOrder: 2, SourceID: 29},
+							{ID: 1, Name: "Minnesota Vikings", Score: 8, DisplayOrder: 3, SourceID: 20},
+						},
+					},
+				},
+			},
+		},
+	}
+	for i, test := range requestScoreCategoryTests {
+		jsonFunc := func(urlPath string) string {
+			return test.teamsJSON
+		}
+		r := newMockHTTPRequestor(jsonFunc)
+		nflTeamRequestor := nflTeamRequestor{requestor: r}
+		got, err := nflTeamRequestor.RequestScoreCategory(db.PlayerTypeNflTeam, 2019, test.friends, test.players)
+		switch {
+		case test.wantErr:
+			if err == nil {
+				t.Errorf("Test %v: wanted error but did not get one", i)
+			}
+		case err != nil:
+			t.Errorf("Test %v: unexpected error: %v", i, err)
+		case !reflect.DeepEqual(test.want, got):
+			t.Errorf("Test %v: Not equal:\nWanted: %v\nGot:    %v", i, test.want, got)
+		}
+	}
+}
 
 func TestNflTeamWins(t *testing.T) {
 	nflTeamWinsTests := []struct {
