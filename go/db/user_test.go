d3348e0421ebb22ad45c64596059b5f9b3abec9e
Jacob Patterson <jacob.patterson@gmail.com>
2019-10-10 13:15:20 -0700

    adding TestGetUserPassword

--- go/db/user_test.go
+++ go/db/user_test.go
@@ -1,6 +1,57 @@
 package db
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
+
+func TestGetUserPassword(t *testing.T) {
+	type userPasswordQueryRow struct {
+		Password string
+	}
+	getUserPasswordTests := []struct {
+		username    string
+		row         userPasswordQueryRow
+		queryRowErr error
+	}{
+		// {},
+		{
+			row: userPasswordQueryRow{
+				Password: "voodoo_cookie73",
+			},
+		},
+		{
+			queryRowErr: errors.New("scan error"),
+		},
+	}
+	for i, test := range getUserPasswordTests {
+		db = mockDatabase{
+			QueryRowFunc: func(query string, args ...interface{}) row {
+				return mockRow{
+					ScanFunc: func(dest ...interface{}) error {
+						switch {
+						case test.queryRowErr != nil:
+							return test.queryRowErr
+						default:
+							return mockRowScanFunc(test.row, dest...)
+						}
+					},
+				}
+			},
+		}
+		gotPassword, gotErr := getUserPassword(test.username)
+		switch {
+		case gotErr != nil:
+			if !errors.Is(gotErr, test.queryRowErr) {
+				t.Errorf("Test %v: wanted error to have %v; got %v", i, test.queryRowErr, gotErr)
+			}
+		default:
+			if gotPassword != test.row.Password {
+				t.Errorf("Test %v: wanted %v; got %v", i, test.row.Password, gotPassword)
+			}
+		}
+	}
+}
 
 func TestPasswordValid(t *testing.T) {
 	passwordIsValidTests := []struct {
