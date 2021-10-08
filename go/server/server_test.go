c14d9ceedd774265015fa1bc16a1785385fb8cdc
Jacob Patterson <jacob.patterson@gmail.com>
2021-10-08 15:50:43 -0700

    adding server handler test

--- go/server/server_test.go
+++ go/server/server_test.go
@@ -1,11 +1,15 @@
 package server
 
 import (
+	"io"
 	"io/ioutil"
 	"log"
+	"net/http"
+	"net/http/httptest"
 	"reflect"
 	"testing"
 	"testing/fstest"
+	"time"
 
 	"github.com/jacobpatterson1549/nate-mlb/go/db"
 )
@@ -139,20 +143,117 @@ func TestNewConfig(t *testing.T) {
 	}
 }
 
-// TODO
-// func TestRunServer(t *testing.T) {
-// 	ds := mockServerDatastore{
-// 		etlDatastore: mockEtlDatastore{
-// 			SportTypesFunc: func() db.SportTypeMap {
-// 				return db.SportTypeMap{
-// 					1: {URL: "st_1_url"},
-// 				}
-// 			},
-// 		},
-// 	}
-// 	logRequestURIs := false
-// 	log := log.New(ioutil.Discard, "test", log.LstdFlags)
-// 	htmlFS := fstest.MapFS{}
-// 	cfg, _ := NewConfig("serverName", ds, 0, "nflAppKey", logRequestURIs, log, htmlFS)
-// 	Run
-// }
+func TestServerHandlers(t *testing.T) {
+	if testing.Short() {
+		t.Skip("uses networking")
+	}
+	tests := []struct {
+		wantCode int
+		method   string
+		path     string
+	}{
+		{wantCode: 200, method: "GET", path: "/"},
+		{wantCode: 200, method: "GET", path: "/favicon.ico"},
+		{wantCode: 200, method: "GET", path: "/robots.txt"},
+		{wantCode: 404, method: "GET", path: "/main.css"},
+		// {wantCode: 200, method: "GET", path: "/about"}, // TODO: do not make live http requests
+		{wantCode: 200, method: "GET", path: "/st_1_url"},
+		{wantCode: 200, method: "GET", path: "/st_1_url/export"},
+		{wantCode: 200, method: "GET", path: "/st_1_url/admin"},
+		{wantCode: 200, method: "GET", path: "/st_1_url/admin/search?q=name&pt=1"},
+		// {wantCode: 303, method: "POST", path: "/st_1_url/admin?action=password"}, // TODO: add Location header to response
+	}
+	for i, test := range tests {
+		ds := mockServerDatastore{
+			GetYearsFunc: func(st db.SportType) ([]db.Year, error) {
+				return nil, nil
+			},
+			adminDatastore: mockAdminDatastore{
+				IsCorrectUserPasswordFunc: func(username string, p db.Password) (bool, error) {
+					return true, nil
+				},
+				SetUserPasswordFunc: func(username string, p db.Password) error {
+					return nil
+				},
+			},
+			etlDatastore: mockEtlDatastore{
+				GetStatFunc: func(st db.SportType) (*db.Stat, error) {
+					return nil, nil
+				},
+				GetFriendsFunc: func(st db.SportType) ([]db.Friend, error) {
+					return nil, nil
+				},
+				GetPlayersFunc: func(st db.SportType) ([]db.Player, error) {
+					return nil, nil
+				},
+				PlayerTypesFunc: func() db.PlayerTypeMap {
+					return nil
+				},
+				SportTypesFunc: func() db.SportTypeMap {
+					return db.SportTypeMap{
+						1: {URL: "st_1_url"},
+					}
+				},
+				GetUtcTimeFunc: func() time.Time {
+					return time.Time{}
+				},
+			},
+		}
+		port := "0"
+		logRequestURIs := false
+		log := log.New(ioutil.Discard, "test", log.LstdFlags)
+		htmlFS := fstest.MapFS{
+			"html/main/main.html": &fstest.MapFile{
+				Data: []byte(
+					`<html>
+	<head>
+		<style>{{template "main.css"}}</style>
+	</head>
+	<body>
+		<p>Tab {{ template "tab.html" }}</p>
+	</body>
+</html>`),
+			},
+			"html/home/tab.html":  &fstest.MapFile{Data: []byte(`1`)},
+			"html/about/tab.html": &fstest.MapFile{Data: []byte(`2`)},
+			"html/stats/tab.html": &fstest.MapFile{Data: []byte(`3`)},
+			"html/admin/tab.html": &fstest.MapFile{Data: []byte(`4`)},
+		}
+		jsFS := fstest.MapFS{}
+		staticFS := fstest.MapFS{
+			"static/main.css": &fstest.MapFile{
+				Data: []byte(`html{color:red;}`),
+			},
+			"static/favicon.ico": &fstest.MapFile{
+				Data: []byte(`[binary-icon]`),
+			},
+			"static/robots.txt": &fstest.MapFile{
+				Data: []byte(``),
+			},
+		}
+		cfg, _ := NewConfig("serverName", ds, port, "dummyNflAppKey", logRequestURIs, log, htmlFS, jsFS, staticFS)
+		h := cfg.handler()
+		ts := httptest.NewTLSServer(h)
+		defer ts.Close()
+		client := ts.Client()
+		path := ts.URL + test.path
+		var resp *http.Response
+		var err error
+		switch test.method {
+		case "GET":
+			resp, err = client.Get(path)
+		case "POST":
+			resp, err = client.Post(path, "dummy_content_type", nil)
+		default:
+			t.Errorf("Test %v: unwanted method: %v", i, test.method)
+		}
+		switch {
+		case err != nil:
+			t.Errorf("Test %v: unwanted error requesting %v: %v", i, path, err)
+		case resp.StatusCode != test.wantCode:
+			body, err := io.ReadAll(resp.Body)
+			resp.Body.Close()
+			t.Errorf("Test %v: wanted %v, got %v:\nresponse: %v\nbody read error: %v\nbody: %s", i, test.wantCode, resp.StatusCode, resp, err, body)
+		}
+	}
+}
